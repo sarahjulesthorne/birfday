b062280b5a92ee3a0d36290854da9d13dd26f767
sarahjulesthorne <sarah.jules.thorne@gmail.com>
2019-05-20 21:31:31 -0500

    WIp

---
+++ src/javascripts/components/myNavBar/my-nav-bar.js
@@ -0,0 +1,14 @@
+const navBarEvents = () => {
+  const navLinks = Array.from(document.getElementsByClassName('navLink'));
+  navLinks.forEach((navLink) => {
+navLink.addEventListener('click', () => {
+  if (else.target.id === '') {
+firebase.auth();
+
+  }
+});
+  });
+console.error('testing nav bar');
+};
+
+export default { navBarEvents, };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase/app';
 import apiKeys from './helpers/apiKeys.json';
-import offFunctions from './components/off/off';
+import authFunctions from './components/auth/auth';
 import birfdayFunctions from './components/birfday/birfday';
+import navBarEvents from './components/myNavBar/my-nav-bar';
 import 'bootstrap';
 import '../styles/main.scss';
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
-  offFunctions.offBuilder();
+  navBarEvents();
+  authFunctions.authBuilder();
   birfdayFunctions.birfdayBuilder();
 };
 init();
